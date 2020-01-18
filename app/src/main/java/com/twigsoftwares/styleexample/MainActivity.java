dae8f32f5d53167998cf6d7e330470e9b75b3260
Mohini Shinde <mohini@twigsoftwares.com>
2020-01-18 12:37:45 +0530

    first commit

---
+++ app/src/main/java/com/twigsoftwares/styleexample/MainActivity.java
@@ -0,0 +1,26 @@
+package com.twigsoftwares.styleexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnNext = findViewById(R.id.btnnext);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
